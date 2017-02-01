592b67a3db3135554dd73abdeb90dba585f1f039
Liz Kerber <emkerber@outlook.com>
2017-02-01 00:13:13 -0600

    Beginning to implement Twilio

--- server.js
+++ server.js
@@ -1,25 +1,38 @@
+var http = require('http');
 var express = require('express');
-var app = express();
 var bodyParser = require('body-parser');
 var path = require('path');
 var Mopidy = require('mopidy');
+var twilio = require('twilio');
 
-// Express route
-var mopidyRoute = require('./routes/mopidyRoute');
-
-
+var app = express();
 
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
-
 
 app.get('/', function(request, response) {
   response.sendFile(path.join(__dirname, 'public/index.html'));
 });
 
-// app.use('/new', mopidyRoute);
+// Express route
+var mopidyRoute = require('./routes/mopidyRoute');
+
+// to configure mopidy
 app.use('/', mopidyRoute);
 
+// to configure twilio
+var trackRequest = '';
+
+app.get('/sms', function(request, response) {
+  var twilio = require('twilio');
+  var twiml = new twilio.TwimlResponse();
+  twiml.response('');
+  console.log('inside twilio function');
+  trackRequest = request.body.Body;
+  app.post('/new', trackRequest);
+});
+// end twilio config
 
 var port = 3000;
 var server = app.listen(port, function() {
