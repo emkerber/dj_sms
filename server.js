10e0f3ce6cb0da628df0d9f587052efd173891ce
Liz Kerber <emkerber@outlook.com>
2017-02-21 22:10:58 -0600

    Still trying to define trackRequest

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.use('/', mopidyRoute);
 app.get('/sms', function(request, response) {
   var twilio = require('twilio');
   console.log('inside twilio function');
-  var trackRequest = request;
+  var trackRequest = request.query.Body;
   console.log('trackRequest:', trackRequest);
   app.post('/new', function(response) {
     response.send(trackRequest);
